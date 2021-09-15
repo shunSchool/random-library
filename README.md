b58cf11f43e3728ffa9898ad69e6908438775357
shunSchool <72032942+shunSchool@users.noreply.github.com>
2021-09-15 11:31:24 -0600

    random library start

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # random-library
+#deez nuts
---
+++ random library.js
@@ -0,0 +1,9 @@
+// library of random functions
+
+function randomDec(low, high) {
+    return Math.random() * (high - low) + low;
+}
+
+function randomInt(low, high)  {
+    return Math.floor(Math.random) * (high - low) + low;
+}
