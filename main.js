b5f575d8ae85673cec57c36706d10dfdf4ab581a
shunSchool <72032942+shunSchool@users.noreply.github.com>
2021-09-17 11:25:23 -0600

    distance calc start

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
+// Add Event Listener to Button
+document.getElementById("calc-btn").addEventListener("click", buttonHandler);
 
-document.addEventListener("click", runCode);
-
-function runCode() {
-    let h1E1 = document.getElementById("output");
-
-  h1E1.innerHTML = randomInt(50, 90)
-  h1E1.style.color = randomRGB();
+function buttonHandler() {
+  // Input
+  let num1 = +document.getElementById("num1-in").value;
+  let num2 = +document.getElementById("num2-in").value;
+  let num3 = +document.getElementById("num3-in").value;
+  let num4 = +document.getElementById("num4-in").value;
+  // Output
+  document.getElementById("output").innerHTML = sum(num1, num2);
+}
+//deez nuts
+function Distance(x1, y1, x2, y2) {
+  return a + b;
 }
