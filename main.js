9aee63c7231f332052cca9be31de896c984a4749
shunSchool <72032942+shunSchool@users.noreply.github.com>
2021-09-20 11:13:04 -0600

    area 1

--- main.js
+++ main.js
@@ -6,11 +6,21 @@ function buttonHandler() {
   let num1 = +document.getElementById("num1-in").value;
   let num2 = +document.getElementById("num2-in").value;
   let num3 = +document.getElementById("num3-in").value;
-  let num4 = +document.getElementById("num4-in").value;
   // Output
   document.getElementById("output").innerHTML = sum(num1, num2);
 }
-//deez nuts
-function Distance(x1, y1, x2, y2) {
-  return a + b;
+//Functions
+
+function area(a, b, h) {
+
+}
+
+function calculate() {
+  //get inputs
+  let num1 = +inputEl1.value;
+  let num2 = +inputEl2.value;
+  let num3 = +inputEl3.value;
+
+  //calculate & output
+  HTMLOutputElement.innerHTML = formulaFunction(num1, num2, num3);
 }
